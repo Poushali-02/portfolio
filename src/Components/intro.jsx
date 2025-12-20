9b1d23e547c1df302fd91fb1e3a42565b67983e0
Poushali Bhattacharyya <poushalibhattacharyya02@gmail.com>
2025-12-20 16:10:57 +0530

    fixed jsx

--- src/Components/intro.jsx
+++ src/Components/intro.jsx
@@ -16,14 +16,14 @@ function Intro() {
       </nav>
       <div className="portfolio-main">
         <div className="portfolio-left">
-          <div className="portfolio-name-box">
-            <h1 className="portfolio-name">Poushali</h1>
-            <h1 className="portfolio-name">Bhattacharyya</h1>
+          <div className="intro-name-box">
+            <h1 className="intro-name">Poushali</h1>
+            <h1 className="intro-name">Bhattacharyya</h1>
           </div>
-          <div className="portfolio-subtitle">A PASSIONATE AI DEVELOPER</div>
+          <div className="intro-subtitle">A PASSIONATE AI DEVELOPER</div>
         </div>
         <div className="portfolio-right">
-          <div className="portfolio-profile-pic">
+          <div className="intro-profile-pic">
             <img src={meImg} alt="Profile" />
           </div>
         </div>
