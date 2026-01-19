f582209be79320cc3cde727fccd05f692f646625
Poushali Bhattacharyya <poushalibhattacharyya02@gmail.com>
2026-01-19 23:07:07 +0530

    added projects

--- src/Components/elements/allProjects.js
+++ src/Components/elements/allProjects.js
@@ -14,6 +14,32 @@ const projects = [
     github: "https://github.com/Poushali-02/Expense-Tracker-MCP-Server",
   },
   {
+    title: "UI/UX concept for a restaurant website",
+    description:
+      "A modern, responsive website concept designed to help a local restaurant improve online presence, showcase menu, location, reviews, and enable easy ordering through delivery platforms.",
+    technologies: ["ReactJs", "TailwindCSS"],
+    link: "https://little-darjeeling.vercel.app/",
+    github: "https://github.com/Poushali-02/web"
+  },
+  {
+    title: "Ramen Art",
+    description:
+    `AI-Powered Story Creation: Generate complete stories with customizable parameters including genre, tone, length, and target audience. Save, view, and organize all your generated stories in one place. Each story includes title, genre, synopsis, and detailed content. User-Specific Stories: All stories are tied to your account for privacy and organization`,
+  
+    technologies: ["ReactJs", "FastAPI", "Gemini API", "Pydantic"],
+    link: "",
+    github: "https://github.com/ramenartai/Ramen_Art"
+  },
+  {
+    title: "FstAPI CRUD Ops",
+    description:
+    `For a Medium blog, A simple RESTful API built with FastAPI to perform CRUD (Create, Read, Update, Delete) operations on a book collection. This project demonstrates the fundamentals of building APIs with FastAPI, including data persistence using JSON file storage.`,
+  
+    technologies: ["Pydantic", "FastAPI"],
+    link: "",
+    github: "https://github.com/ramenartai/Ramen_Art"
+  },
+  {
     title: "Fake News Detector", // 2
     description: `
             A web application that analyzes news 
@@ -66,14 +92,6 @@ const projects = [
     technologies: ["pandas", "random-forest", "numpy"],
     link: "https://whattogrow.onrender.com/",
     github: "https://github.com/Poushali-02/crop-prediction-ml/",
-  },
-  {
-    title: "UI/UX concept for a restaurant website",
-    description:
-      "A modern, responsive website concept designed to help a local restaurant improve online presence, showcase menu, location, reviews, and enable easy ordering through delivery platforms.",
-    technologies: ["ReactJs", "TailwindCSS"],
-    link: "https://little-darjeeling.vercel.app/",
-    github: "https://github.com/Poushali-02/web"
-  },
+  }
 ];
 export default projects;
